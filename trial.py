49e02f9ee16093a92f9349d36bfee180e09446b7
Arjun-3105 <arjun.chaudhary3105@gmail.com>
2024-11-17 15:55:37 +0530

    final

---
+++ trial.py
@@ -0,0 +1,9 @@
+import cv2
+import mediapipe
+import numpy as np
+import flask
+
+print(f"OpenCV version: {cv2.__version__}")
+print(f"Mediapipe version: {mediapipe.__version__}")
+print(f"Numpy version: {np.__version__}")
+print(f"Flask version: {flask.__version__}")
